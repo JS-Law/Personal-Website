313101951ba9a06ebace7b082793e7c58e06c3f7
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2025-06-02 19:45:50 -0400

    fix: update asset paths to use lowercase directory name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
-import profilePicture from '../Assets/pictureOfMe.png'
+import profilePicture from '../assets/pictureOfMe.png'
 export default function Header() {
   return (
     <header className="header">
